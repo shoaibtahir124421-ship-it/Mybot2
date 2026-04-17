511963cc180a6fa7501dd8f909c70a032ceec62a
root <root@localhost.localdomain>
2026-04-17 18:59:52 +0000

    debug bot token

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("AAFeD6Ye5FMNTiBZNDDHZErk25EeMMRj9Os")
 print("TOKEN:", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
