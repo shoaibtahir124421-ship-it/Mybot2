6b8feaa3499a3aa00e811412bbdd5cf34f8da0ee
root <root@localhost.localdomain>
2026-04-17 18:49:20 +0000

    debug bot token

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
+print("TOKEN:", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot chal raha hai 🚀")
